95d6c17d18ffa4fe7174d04d4e1b69b7755cb21c
DarkRise12 <darkrisenik@gmail.com>
2018-02-21 17:52:45 +0700

    Added chess_code.cpp with chessboard

---
+++ chess_code.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+printf("  a b c d e f g h  ");
+printf("8 л к с к ф с к л 8");
+printf("7 п п п п п п п п 7");
+printf("6                 6");
+printf("5                 5");
+printf("4                 4");
+printf("3                 3");
+printf("2 п п п п п п п п 2");
+printf("1 л к с к ф с к л 1");
+printf("  a b c d e f g h  ");
+
+}
